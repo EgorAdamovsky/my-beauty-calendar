6f60219d08c72d6706e60d190f1571c875d33d9b
EGORKA <adamovskiy.y@pdu.by>
2024-01-03 20:49:42 +0300

    ver 1.6
    out -> for desktop with footer

--- my-beauty-calendar.py
+++ my-beauty-calendar.py
@@ -24,13 +24,16 @@ mycfg.close()
 
 # НАСТРОЙКИ
 imsize = (1920, 1080)
-offset = (1147, 100)
-cellsize = 32
-tabsize = 256
+offset = (1213, 160)
+yearplace = (1440, 80)
+cellsize = 28
+tabsize = 230
 tabpad = 16
 table = 3
-fontsize = 0.5
-font = cv2.FONT_HERSHEY_COMPLEX
+fontsize = 0.4
+fontnums = cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+fontyear = cv2.FONT_HERSHEY_TRIPLEX
+fontmes = cv2.FONT_HERSHEY_COMPLEX
 m = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 
 # ПОДГОТОВКА
@@ -59,7 +62,7 @@ for moon in range(12):
             if day != 0:
                 if dofw > 5:
                     lay = cv2.circle(lay,
-                                     (offset[0] + tabsize * line + cellsize * j + int(cellsize / 3),
+                                     (offset[0] + tabsize * line + cellsize * j + int(cellsize / 3.5),
                                       offset[1] + tabsize * row + cellsize * i - int(cellsize / 6)),
                                      int(30 * fontsize), endcol, -1)
             j += 1
@@ -73,15 +76,19 @@ for moon in range(12):
 img = cv2.addWeighted(img, 1, lay, over, 0)
 
 # ТЕКСТ
-cv2.putText(img, str(year), (50, 1030), cv2.FONT_HERSHEY_TRIPLEX, 3, fontcol)
+cv2.putText(img, str(year), yearplace, fontyear, 2.5, fontcol)
 line, row, i, j, ch = 0, 0, 0, 0, 0
 for moon in range(12):
     month = cal.monthdayscalendar(year, moon + 1)
+
+    # месяцы
     for week in month:
         cv2.putText(img, m[moon],
                     (tabsize * line + offset[0],
                      tabsize * row + offset[1] - cellsize),
-                    font, fontsize, fontcol)
+                    fontmes, 1.5 * fontsize, fontcol)
+
+        # дни
         for day in week:
             ch += 1
 
@@ -93,13 +100,13 @@ for moon in range(12):
                 cv2.putText(img, str(day),
                             (offset[0] + tabsize * line + cellsize * j + fullday,
                              offset[1] + tabsize * row + cellsize * i),
-                            font, fontsize, fontcol)
+                            fontnums, fontsize, fontcol)
 
             # текущий день
             if nowpoint == "true":
                 if ch == curday and moon + 1 == curmonth and curyear == year:
                     img = cv2.circle(img,
-                                     (offset[0] + tabsize * line + cellsize * j + int(cellsize / 3),
+                                     (offset[0] + tabsize * line + cellsize * j + int(cellsize / 3.5),
                                       offset[1] + tabsize * row + cellsize * i - int(cellsize / 6)),
                                      int(28 * fontsize), nowcol, 2)
             j += 1
@@ -117,4 +124,10 @@ cv2.imwrite(fout, img)
 
 # УСТАНОВКА НА РАБОЧИЙ СТОЛ
 ctypes.windll.user32.SystemParametersInfoW(20, 0, os.path.abspath(os.curdir) + "\\" + fout, 0)
+
+# ТЕСТ
+# img = cv2.resize(img, (960, 540))
+# cv2.imshow("", img)
+# cv2.waitKey()
+
 print("Готово!")
