96b7872e6021e78f15a4d5f5b2850069f4f84520
EGORKA <adamovskiy.y@pdu.by>
2024-01-07 01:53:39 +0300

    ver 1.11
    add holidays file
    update readme

--- my-beauty-calendar.py
+++ my-beauty-calendar.py
@@ -23,6 +23,7 @@ nowpoint = data[7]
 roundbias = int(data[8])
 blur = int(data[9])
 fordesktop = data[10]
+noworks = data[11]
 mycfg.close()
 
 # НАСТРОЙКИ
@@ -41,6 +42,7 @@ fontmes = cv2.FONT_HERSHEY_COMPLEX
 m = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
 
 # ПОДГОТОВКА
+noworksdaysint = []
 curday = datetime.datetime.now().day
 curmonth = datetime.datetime.now().month
 curyear = datetime.datetime.now().year
@@ -48,12 +50,17 @@ img = cv2.imread(file)
 img = cv2.resize(img, imsize, img)
 lay = np.zeros(shape=[imsize[1], imsize[0], 3], dtype=np.uint8)
 cal = calendar.TextCalendar()
+noworksdays = open(noworks, "r").readlines()
+for i in range(len(noworksdays)):
+    noworksdays[i] = noworksdays[i].strip()
+    temp = noworksdays[i].split('.')
+    noworksdaysint.append([int(temp[0]), int(temp[1])])
 
 # ФОН И ВЫХОДНЫЕ ДНИ
 line, row, i, j = 0, 0, 0, 0
 for moon in range(12):
 
-    # месяцы
+    # плитки месяцев
     month = cal.monthdayscalendar(year, moon + 1)
     x1 = offset[0] + tabsize * line + tabpad - cellsize
     y1 = offset[1] + tabsize * row + tabpad - int(2.5 * cellsize)
@@ -66,16 +73,21 @@ for moon in range(12):
     lay = cv2.circle(lay, (x2 - roundbias, y1 + roundbias), roundbias, backcol, -1)
     lay = cv2.circle(lay, (x2 - roundbias, y2 - roundbias), roundbias, backcol, -1)
 
-    # дни недели
-    for week in month:
-        dofw = 0
-        for day in week:
-            dofw += 1
-            if day != 0:
-                if dofw > 5:
-                    wx = offset[0] + tabsize * line + cellsize * j + int(cellsize / 3.5)
-                    wy = offset[1] + tabsize * row + cellsize * i - int(cellsize / 6)
-                    lay = cv2.circle(lay, (wx, wy), int(30 * fontsize), endcol, -1)
+    # кружочки выходных и праздников
+    for week in month:  # цикл по неделям месяца
+        dofw = 0  # счетчик дня недели
+        for day in week:  # цикл по дням недели
+            dofw += 1  # инкремент счетчика дня недели
+            if day != 0:  # если текущий день присутствует в месяце
+                holiday = False  # по умолчанию день рабочий
+                for nwd in noworksdaysint:  # цикл по праздничным дням
+                    if nwd[0] == day and nwd[1] == moon + 1:  # если текущий день месяца праздничный
+                        holiday = True  # ставится отметка
+                        break  # дальнейшая проверка не требуется
+                if dofw > 5 or holiday:  # если день выходной или праздничный
+                    wx = offset[0] + tabsize * line + cellsize * j + int(cellsize / 3.5)  # смещение кружка по X
+                    wy = offset[1] + tabsize * row + cellsize * i - int(cellsize / 6)  # смещение кружка по Y
+                    lay = cv2.circle(lay, (wx, wy), int(30 * fontsize), endcol, -1)  # отрисовка кружка
             j += 1
         i += 1
         j = 0
